bcac822d98adc3d1cb93123be4672b5660cab9c3
kingdido999 <kingdido999@gmail.com>
2016-03-12 09:52:17 -0500

    improve view; update food generation algorithm

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const FPS_MIN = 6;
 const FPS_MAX = 24;
 const CHUNK_WIDTH = 40;
 const CHUNK_HEIGHT = 40;
-const SNAKE_HEAD_COLOR = '#1e6823';
-const SNAKE_BODY_COLOR = '#44a340';
-const FOOD_COLOR = 'red';
+const BACKGROUND_COLOR = '#fff';
+const CANVAS_COLOR = '#ee334d';
+const SNAKE_HEAD_COLOR = '#fff';
+const SNAKE_BODY_COLOR = '#fff';
+const FOOD_COLOR = '#fff';
 const DIR = {
   left: 'Left',
   right: 'Right',
@@ -15,16 +17,12 @@ const DIR = {
   down: 'Down'
 };
 
-var s, f, t;
-var fps = FPS_MIN;
-var isPaused = false;
-var stats = {
-  score: 0
-}
+var t;
 
 // Building block of snake body and food
 var chunk = function(spec) {
   var that = {};
+  var padding = spec.padding || 2;
 
   that.getX = function() { return spec.x; }
   that.getY = function() { return spec.y; }
@@ -34,7 +32,7 @@ var chunk = function(spec) {
   that.setColor = function(color) { spec.color = color; }
   that.draw = function() {
     ctx.fillStyle = spec.color;
-    ctx.fillRect(spec.x, spec.y, spec.width, spec.height);
+    ctx.fillRect(spec.x+padding, spec.y+padding, spec.width-padding*2, spec.height-padding*2);
   };
   that.clear = function() {
     ctx.clearRect(spec.x, spec.y, spec.width, spec.height);
@@ -44,6 +42,19 @@ var chunk = function(spec) {
 
 var food = function(spec) {
   var that = chunk(spec);
+  var padding = spec.padding || 2;
+
+  that.draw = function() {
+    ctx.strokeStyle = spec.color;
+    ctx.strokeRect(spec.x+padding, spec.y+padding, spec.width-padding*2, spec.height-padding*2);
+    ctx.beginPath();
+    ctx.moveTo(spec.x+padding, spec.y+padding);
+    ctx.lineTo(spec.x+spec.width-padding, spec.y+spec.height-padding);
+    ctx.moveTo(spec.x+spec.width-padding, spec.y+padding);
+    ctx.lineTo(spec.x+padding, spec.y+spec.height-padding);
+    ctx.closePath();
+    ctx.stroke();
+  }
   return that;
 }
 
@@ -69,158 +80,204 @@ var snake = function(spec) {
   that.addHead = function(chunk) {
     spec.queue.push(chunk);
   }
+  that.containsChunk = function(chunk) {
+    for (var i = 0; i < spec.queue.length; i++) {
+      if (spec.queue[i].getX() === chunk.getX() && spec.queue[i].getY() === chunk.getY()) return true;
+    }
+
+    return false;
+  }
 
   return that;
 }
 
-function init() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+var game = function(spec) {
+  var that = {};
+  var isPaused = spec.isPaused || false;
+  var fps = spec.fps || FPS_MIN;
+  var stats = {
+    score: 0
+  }
+  var bgColor = spec.bgColor || BACKGROUND_COLOR;
+  var canvasColor = spec.canvasColor || CANVAS_COLOR;
+  var snakeHeadColor = spec.snakeHeadColor || SNAKE_HEAD_COLOR;
+  var snakeBodyColor = spec.snakeBodyColor || SNAKE_BODY_COLOR;
+  var foodColor = spec.foodColor || FOOD_COLOR;
+  var s, f;
 
-  var headPos = getRandPos(canvas.width, canvas.height, CHUNK_WIDTH, CHUNK_HEIGHT);
-  var foodPos = getRandPos(canvas.width, canvas.height, CHUNK_WIDTH, CHUNK_HEIGHT);
-
-  var head = chunk({
-    x: headPos.x,
-    y: headPos.y,
-    width: CHUNK_WIDTH,
-    height: CHUNK_HEIGHT,
-    color: SNAKE_HEAD_COLOR
-  });
-
-  // Assuming the snake has at least two chunks
-  // at the very beginning
-  s = snake({
-    speed: CHUNK_WIDTH,
-    dir: null,
-    queue: [head]
-  });
-
-  f = food({
-    x: foodPos.x,
-    y: foodPos.y,
-    width: CHUNK_WIDTH,
-    height: CHUNK_HEIGHT,
-    color: FOOD_COLOR
-  });
-
-  // Draw snake and food
-  s.draw();
-  f.draw();
-
-  document.getElementById('score').innerHTML = stats.score;
-}
-
-function loop() {
-  t = new Timer(loop, 1000 / fps);
-
-  f.draw();
-
-  // Calculate x and y translation by current speed and direction
-  var transX = 0, transY = 0;
-  var speed = s.getSpeed();
-  var oldHead = s.getHead();
-
-  // Boundaries check before apply translation
-  switch (s.getDirection()) {
-    case DIR.left:
-      if (oldHead.getX() - speed < 0) return gameover();
-      else transX -= speed;
-      break;
-    case DIR.right:
-      if (oldHead.getX() + oldHead.getWidth() + speed > canvas.width) return gameover();
-      else transX += speed;
-      break;
-    case DIR.up:
-      if (oldHead.getY() - speed < 0) return gameover();
-      else transY -= speed;
-      break;
-    case DIR.down:
-      if (oldHead.getY() + oldHead.getHeight() + speed > canvas.height) return gameover();
-      else transY += speed;
-      break;
-    default:
-
+  that.getFPS = function() {
+    return fps;
   }
 
-  // Detect snake body collision
-  for (var i = 0; i < s.getBody().length; i++) {
-    var c = s.getBody()[i];
-    if (oldHead.getX() + transX == c.getX() && oldHead.getY() + transY == c.getY()) {
-      console.log('Ouch!')
-      return gameover();
-    };
+  that.getSnake = function() {
+    return s;
   }
 
-  // Detect food collision
-  if (oldHead.getX() + transX == f.getX() && oldHead.getY() + transY == f.getY()) {
-    console.log('Eat');
-    stats.score += 1;
-    document.getElementById('score').innerHTML = stats.score;
+  that.getFood = function() {
+    return f;
+  }
 
-    // Speed up
-    if (fps < FPS_MAX) fps += (FPS_MAX/fps)*0.05;
-    f.clear();
-    f = makeFood();
+  that.init = function() {
+    canvas.style.backgroundColor = canvasColor;
+    document.body.style.backgroundColor = bgColor;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+    var headPos = getRandPos(canvas.width, canvas.height, CHUNK_WIDTH, CHUNK_HEIGHT);
+    var foodPos = getRandPos(canvas.width, canvas.height, CHUNK_WIDTH, CHUNK_HEIGHT);
+
+    var head = chunk({
+      x: headPos.x,
+      y: headPos.y,
+      width: CHUNK_WIDTH,
+      height: CHUNK_HEIGHT,
+      color: snakeHeadColor
+    });
+
+    s = snake({
+      speed: CHUNK_WIDTH,
+      dir: null,
+      queue: [head]
+    });
+
+    f = food({
+      x: foodPos.x,
+      y: foodPos.y,
+      width: CHUNK_WIDTH,
+      height: CHUNK_HEIGHT,
+      color: foodColor
+    });
+
+    // Draw snake and food
+    s.draw();
     f.draw();
-  } else {
-    s.removeTail().clear();
+
+    document.getElementById('score').innerHTML = stats.score;
   }
 
-  // Add a new head to the queue of chunks
-  var newHead = chunk({
-    x: oldHead.getX() + transX,
-    y: oldHead.getY() + transY,
-    width: oldHead.getWidth(),
-    height: oldHead.getHeight(),
-    color: SNAKE_HEAD_COLOR
-  });
+  that.loop = function() {
+    t = new Timer(that.loop, 1000 / fps);
 
-  s.addHead(newHead);
-  newHead.draw();
+    // Calculate x and y translation by current speed and direction
+    var transX = 0, transY = 0;
+    var speed = s.getSpeed();
+    var oldHead = s.getHead();
 
-  // Old head now becomes a part of body
-  if (s.getLength() > 1) {
-    oldHead.setColor(SNAKE_BODY_COLOR);
-    oldHead.draw();
+    // Boundaries check before apply translation
+    switch (s.getDirection()) {
+      case DIR.left:
+        if (oldHead.getX() - speed < 0) return gameover();
+        else transX -= speed;
+        break;
+      case DIR.right:
+        if (oldHead.getX() + oldHead.getWidth() + speed > canvas.width) return gameover();
+        else transX += speed;
+        break;
+      case DIR.up:
+        if (oldHead.getY() - speed < 0) return gameover();
+        else transY -= speed;
+        break;
+      case DIR.down:
+        if (oldHead.getY() + oldHead.getHeight() + speed > canvas.height) return gameover();
+        else transY += speed;
+        break;
+      default:
+
+    }
+
+    // Detect snake body collision
+    for (var i = 0; i < s.getBody().length; i++) {
+      var c = s.getBody()[i];
+      if (oldHead.getX() + transX == c.getX() && oldHead.getY() + transY == c.getY()) {
+        console.log('Ouch!')
+        return gameover();
+      };
+    }
+
+    // Detect food collision
+    if (oldHead.getX() + transX == f.getX() && oldHead.getY() + transY == f.getY()) {
+      console.log('Eat');
+      stats.score += 1;
+      document.getElementById('score').innerHTML = stats.score;
+
+      // Speed up
+      if (fps < FPS_MAX) fps += (FPS_MAX/fps)*0.05;
+      f.clear();
+      f = makeFood();
+      f.draw();
+    } else {
+      s.removeTail().clear();
+    }
+
+    // Add a new head to the queue of chunks
+    var newHead = chunk({
+      x: oldHead.getX() + transX,
+      y: oldHead.getY() + transY,
+      width: oldHead.getWidth(),
+      height: oldHead.getHeight(),
+      color: snakeHeadColor
+    });
+
+    s.addHead(newHead);
+    newHead.draw();
+
+    // Old head now becomes a part of body
+    if (s.getLength() > 1) {
+      oldHead.setColor(snakeBodyColor);
+      oldHead.draw();
+    }
   }
-}
 
-// Generate a new food
-function makeFood() {
-  var pos = getRandPos(canvas.width, canvas.height, CHUNK_WIDTH, CHUNK_HEIGHT);
-
-  return chunk({
-    x: pos.x,
-    y: pos.y,
-    width: CHUNK_WIDTH,
-    height: CHUNK_HEIGHT,
-    color: FOOD_COLOR
-  });
-}
-
-function pause() {
-  if (isPaused) {
-    console.log('Resume');
-    isPaused = false;
-    t.resume();
-  } else {
-    console.log('Pause');
-    isPaused = true;
-    t.pause();
+  that.pause = function() {
+    if (isPaused) {
+      console.log('Resume');
+      isPaused = false;
+      t.resume();
+    } else {
+      console.log('Pause');
+      isPaused = true;
+      t.pause();
+    }
   }
-}
 
-function gameover() {
-  console.log('Game Over');
-  t.stop();
-  var restart = window.confirm('Play again?');
-  if (restart) {
-    init();
-    t = null;
-    fps = FPS_MIN;
-  } else {
-    window.onkeydown = null;
+  that.resizeCanvas = function() {
+    // Only resize canvas before the game start
+    if (!t) {
+      canvas.width = window.innerWidth - (window.innerWidth % CHUNK_WIDTH) - CHUNK_WIDTH;
+      canvas.height = window.innerHeight - (window.innerHeight % CHUNK_HEIGHT) - CHUNK_HEIGHT;
+      that.init();
+    }
   }
+
+  var makeFood = function() {
+    var pos = getRandPos(canvas.width, canvas.height, CHUNK_WIDTH, CHUNK_HEIGHT);
+    var f = food({
+      x: pos.x,
+      y: pos.y,
+      width: CHUNK_WIDTH,
+      height: CHUNK_HEIGHT,
+      color: foodColor
+    });
+
+    while (s.containsChunk(f)) return makeFood();
+
+    return f;
+  }
+
+  var gameover = function() {
+    console.log('Game Over');
+    t.stop();
+    var restart = window.confirm('Play again?');
+    if (restart) {
+      that.init();
+      t = null;
+      fps = FPS_MIN;
+    } else {
+      window.onkeydown = null;
+    }
+  }
+
+  return that;
 }
 
 function getRandPos(width, height, offsetX, offsetY) {
@@ -264,18 +321,17 @@ function Timer(callback, delay) {
   this.resume();
 }
 
-function resizeCanvas() {
-  // Only resize canvas before the game start
-  if (!t) {
-    canvas.width = window.innerWidth - (window.innerWidth % CHUNK_WIDTH) - CHUNK_WIDTH;
-    canvas.height = window.innerHeight - (window.innerHeight % CHUNK_HEIGHT) - CHUNK_HEIGHT*2;
-    init();
-  }
-}
+
+
+var g = game({});
+g.resizeCanvas();
+
+window.addEventListener('resize', g.resizeCanvas, false);
 
 // Key events listener
 window.onkeydown = function(e) {
   var code = e.keyCode ? e.keyCode : e.which;
+  var s = g.getSnake();
   var currentDir = s.getDirection();
 
   switch (code) {
@@ -292,15 +348,11 @@ window.onkeydown = function(e) {
       if (!currentDir || currentDir === DIR.left || currentDir === DIR.right) s.setDirection(DIR.down);
       break;
     case 32: // Space
-      if (t) pause();
+      if (t) g.pause();
     default:
 
   }
 
   // Start the game after arrow key is pressed
-  if (!t && s.getDirection()) t = new Timer(loop, 1000 / fps);
+  if (!t && s.getDirection()) t = new Timer(g.loop, 1000 / g.getFPS());
 }
-
-window.addEventListener('resize', resizeCanvas, false);
-
-resizeCanvas();
